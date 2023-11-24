7a4c953eb020096adc778798efb4870e8fa6bf6d
bogdan_mykhailov <bogdan_mykhailov@icloud.com>
2023-11-24 22:59:53 +0200

    init commit

--- src/test/java/RunTests.java
+++ src/test/java/RunTests.java
@@ -7,28 +7,32 @@ import java.util.concurrent.ExecutorService;
 @CucumberOptions(
         features = {"src/test/resources/test.feature"},
         glue = "step",
-        plugin = "json:target/cucumber.json"
-        // plugin = {"pretty", "html:target/cucumber-reports"}
+        // plugin = "json:target/cucumber.json"
+         plugin = {"pretty", "html:target/cucumber-reports"}
 )
 public class RunTests {
+
     public static void main(String[] args) {
         // Отримання значень параметрів системи Maven
         String threadCount = System.getProperty("threadCount", "1");
         String browser = System.getProperty("browser", "chrome");
 
-        // Створюємо пул потоків та виконуємо завдання паралельно
-        ExecutorService executorService = Executors.newFixedThreadPool(Integer.parseInt(threadCount));
-        for (int i = 0; i < Integer.parseInt(threadCount); i++) {
-            final int taskNumber = i;
-            executorService.execute(() -> {
-                System.out.println("Task " + taskNumber + " is running with browser: " + browser
-                        + " in thread " + Thread.currentThread().getName());
-            });
-        }
+        System.out.println("!!!!!!!!Thread Count: " + threadCount);
+        System.out.println("Browser: " + browser);
 
-        executorService.shutdown();
+//        // Створюємо пул потоків та виконуємо завдання паралельно
+//        ExecutorService executorService = Executors.newFixedThreadPool(Integer.parseInt(threadCount));
+//        for (int i = 0; i < Integer.parseInt(threadCount); i++) {
+//            final int taskNumber = i;
+//            executorService.execute(() -> {
+//                System.out.println("Task " + taskNumber + " is running with browser: " + browser
+//                        + " in thread " + Thread.currentThread().getName());
+//            });
+//        }
+//
+//        executorService.shutdown();
+
     }
-
 }
 
 
