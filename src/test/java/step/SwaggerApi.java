ba96de13d73bce5a26d0c4d2a45563c533ff5d84
bogdan_mykhailov <bogdan_mykhailov@icloud.com>
2023-11-27 14:48:09 +0200

    swagger get request

--- src/test/java/step/SwaggerApi.java
+++ src/test/java/step/SwaggerApi.java
@@ -10,18 +10,29 @@ import static org.junit.Assert.assertEquals;
 public class SwaggerApi {
 
     private Response response;
-
+    private String createdUserName;
     @Given("the user has access to the Petstore API")
     public void userHasAccessToPetstoreAPI() {
-        RestAssured.baseURI = "https://petstore.swagger.io/v2";
+
+          RestAssured.baseURI = "https://petstore.swagger.io/v2";
     }
 
     @When("the user sends a GET request to retrieve information about the user with identifier {string}")
     public void userSendsGetRequest(String userName) {
+        createdUserName = userName;
         response = given()
-                .log().all() // логування всіх деталей запиту
+                .log().all()
+                .contentType("application/json")
+                .body("{ \"id\": 0, \"username\": \"" + userName + "\", \"firstName\": \"maryna\"," +
+                        "\"lastName\": \"mykhailova\", \"email\": \"test@gmail.com\", \"password\": \"111\", " +
+                        "\"phone\": \"1234567890\", \"userStatus\": 0 }")
                 .when()
-                .get("/user/" + userName);
+                .post("/user/createWithArray");
+
+//        response = given()
+//                .log().all() // логування всіх деталей запиту
+//                .when()
+//                .get("/user/" + userName);
     }
 
     @Then("the server should return status code {int}")
